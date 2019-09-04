70d0efdca4240021df4a92ba4f27d7c3f609a3a7
Demetris Marnerides <dmarnerides@gmail.com>
2019-09-04 09:50:48 +0100

    Fix cosine eps

--- train.py
+++ train.py
@@ -45,13 +45,12 @@ def parse_args():
 class ExpandNetLoss(nn.Module):
     def __init__(self, loss_lambda=5):
         super(ExpandNetLoss, self).__init__()
-        self.similarity = torch.nn.CosineSimilarity(dim=1, eps=1e-5)
+        self.similarity = torch.nn.CosineSimilarity(dim=1, eps=1e-20)
         self.l1_loss = nn.L1Loss()
         self.loss_lambda = loss_lambda
-        self.eps = 1e-5
 
     def forward(self, x, y):
-        cosine_term = (1 - self.similarity(x + self.eps, y + self.eps)).mean()
+        cosine_term = (1 - self.similarity(x, y)).mean()
         return self.l1_loss(x, y) + self.loss_lambda * cosine_term
 
 
